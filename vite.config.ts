56825b8652d6782462ea004b654859ae476d4ffa
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-04-30 12:34:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['axios'], // 确保 axios 被正确打包
+      // 确保 axios 被正确打包
+      external: ['axios']
     },
   },
   plugins: [react()],
   resolve: {
     alias: {
-      // 确保不使用 Node.js 模块
-      fs: false,
+      // 如果需要兼容 Node.js 模块，可以使用 polyfill
+      fs: 'browserify-fs', // 替换为兼容的浏览器实现
     },
   },
 });
